c40f43c0c037c4bc76b062508e29af141028479c
Jashan <jashan.khela@hotmail.com>
2018-06-11 20:13:31 +0000

    step 5

---
+++ step5/http-functions.js
@@ -0,0 +1,13 @@
+module.exports = function getHTML (options, callback) {
+var https = require('https') ;
+  var arr = []
+  var example = https.get(options , function (response)  {
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      arr.push(data) ;
+    } )
+    response.on('end' , function(data) {
+      callback(arr)
+    })
+  })
+};
