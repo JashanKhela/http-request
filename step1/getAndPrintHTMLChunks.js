39b76aee9320453c62152ad815dadcbc30470924
Jashan <jashan.khela@hotmail.com>
2018-06-11 18:24:08 +0000

    step one is done

---
+++ step1/getAndPrintHTMLChunks.js
@@ -0,0 +1,19 @@
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+  var https = require('https') ;
+  var example = https.get(requestOptions , function (response)  {
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      console.log(data + "/n")
+    } )
+  }
+  )
+
+
+}
+
+getAndPrintHTMLChunks() ;
